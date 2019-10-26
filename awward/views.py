f1ceeb0f1b2e4c8b40cd45bcb5a7ad1806252db8
Ruterana <ncharlene38@gmail.com>
2019-10-26 16:15:14 +0200

    i modify my code

--- awward/views.py
+++ awward/views.py
@@ -32,10 +32,10 @@ def search_results(request):
 
     if 'project' in request.GET and request.GET["project"]:
         search_term = request.GET.get("project")
-        searched_projects = Project.search_by_name(search_term)
+        searched_projects = Project.search_by_project_title(search_term)
         message = f"{search_term}"
 
-        return render(request, 'all-file/search.html',{"message":message,"articles": searched_articles})
+        return render(request, 'all-file/search.html',{"message":message,"projects": searched_projects})
 
     else:
         message = "You haven't searched for any term"
