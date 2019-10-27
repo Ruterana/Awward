84afa3413790806e0f7c9058f5ac3ff2d8ccfb25
Ruterana <ncharlene38@gmail.com>
2019-10-27 22:12:19 +0200

    iadd link of project

--- awward/views.py
+++ awward/views.py
@@ -36,7 +36,7 @@ def search_results(request):
 
     if 'project' in request.GET and request.GET["project"]:
         search_term = request.GET.get("project")
-        searched_projects = Project.search_by_title(search_term)
+        searched_projects = project.search_by_title(search_term)
         message = f"{search_term}"
 
         return render(request, 'all-file/search.html',{"message":message,"projects": searched_projects})
